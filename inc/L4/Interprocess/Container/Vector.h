78ea12eeca03757cc278e5363a95b3833677a2ce
Jerin Philip <jphilip@ed.ac.uk>
2021-07-16 20:50:11 +0000

    boost::interprocess::vector -> std::vector

--- inc/L4/Interprocess/Container/Vector.h
+++ inc/L4/Interprocess/Container/Vector.h
@@ -1,13 +1,17 @@
 #pragma once
 
-#include <boost/interprocess/containers/vector.hpp>
+// #include <boost/interprocess/containers/vector.hpp>
+#include <vector>
 
 namespace L4 {
 namespace Interprocess {
 namespace Container {
 
+// template <typename T, typename Allocator>
+// using Vector = boost::interprocess::vector<T, Allocator>;
+// @jerinphilip: This is probably a bad idea.
 template <typename T, typename Allocator>
-using Vector = boost::interprocess::vector<T, Allocator>;
+using Vector = std::vector<T, Allocator>;
 
 }  // namespace Container
 }  // namespace Interprocess
